f7bb9799def43c47df17406f103f15bcf2e14b4d
nmakes <nav.naveenvenkat@gmail.com>
2018-08-23 18:52:19 +0530

    starting PPG analysis

---
+++ ppg.py
@@ -0,0 +1,9 @@
+'''
+	Written By Naveen Venkat
+	nav.naveenvenkat@gmail.com
+	github.com/nmakes/spoof
+	Final Year Undergraduate, BITS Pilani
+
+	This work is licenced under a Creative Commons Attribution-NonCommercial-ShareAlike 
+	(CC BY-NC-SA) licence. 
+'''
