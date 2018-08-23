55b156c7e0deb8b350c95626d879ba827b63a9cf
nmakes <nav.naveenvenkat@gmail.com>
2018-08-23 19:40:13 +0530

    writing elementary signal cleaning code

--- ppg.py
+++ ppg.py
@@ -1,5 +1,5 @@
 '''
-	Written By Naveen Venkat
+	Written By Naveen Venkat, 2018
 	nav.naveenvenkat@gmail.com
 	github.com/nmakes/spoof
 	Final Year Undergraduate, BITS Pilani
@@ -7,3 +7,19 @@
 	This work is licenced under a Creative Commons Attribution-NonCommercial-ShareAlike 
 	(CC BY-NC-SA) licence. 
 '''
+
+class SignalCleaner:
+
+	def __init__(self):
+		pass
+
+
+	def clean(self, signal):
+		'''
+			Input
+				- signal: vector of size 1xN containing the signal with N timesteps
+			Output
+				- the cleaned signal
+		'''
+
+		pass
